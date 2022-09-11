24a57661f8e319b7ccaabdf17bf0c0b3cf2cf0d3
isack <isack@LAPTOP-KBT7Q9Q0>
2022-09-11 11:47:39 -0700

    Agregar archivos de proyecto.

---
+++ Practica2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Edad = 24;
+            string Nombre = "Isack";
+            string Pais = "España";
+
+            Console.WriteLine("Mi nombre es {0} tengo  de edad {1} y me gustaria visitar {2}.", Nombre,Edad,Pais);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
